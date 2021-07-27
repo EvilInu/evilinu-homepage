d92a25ec122545aa10ed04fc6731c331cb24f636
Edin Hadzovic <e.hadzovic@denovo.at>
2021-07-27 22:46:47 +0200

    init page.

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+export default function () {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        backgroundColor: 'lightcyan',
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <h1>Landswap is under construction</h1>
+      <blockquote>We are working overtime to deliver!</blockquote>
+    </div>
+  );
+}
